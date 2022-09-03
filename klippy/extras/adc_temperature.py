cb3515e8a395fa580ae95ca5f3377218ccf3bac5
Rich Felker <dalias@aerifal.cx>
2022-09-03 18:58:51 +0100

    fast temperature reporting

--- klippy/extras/adc_temperature.py
+++ klippy/extras/adc_temperature.py
@@ -12,7 +12,8 @@ import logging, bisect
 
 SAMPLE_TIME = 0.001
 SAMPLE_COUNT = 8
-REPORT_TIME = 0.300
+#REPORT_TIME = 0.300
+REPORT_TIME = 0.1
 RANGE_CHECK_COUNT = 4
 
 # Interface between ADC and heater temperature callbacks
